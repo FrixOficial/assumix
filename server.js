644edf482db27d0cada72d5be7877f36d0f52bbd
FrixOficial <40075146+FrixOficial@users.noreply.github.com>
2018-09-29 17:31:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -232,20 +232,6 @@ message.author.send(embed);
           message.author.send("hola");
     
     }
-    if (message.content === '-oofsound'){
-        if (message.channel.type !== 'text') return;
-
-        const { voiceChannel } = message.member;
-
-        if (!voiceChannel) {
-            return message.reply('please join a voice channel first!');
-        }
-
-        voiceChannel.join().then(connection => {
-            const stream = ytdl('https://www.youtube.com/watch?v=zJTjOi76pu8', { filter: 'audioonly' });
-            const dispatcher = connection.playStream(stream);
-
-            dispatcher.on('end', () => voiceChannel.leave());
   
   });
 bot.login(process.env.TOKEN);
